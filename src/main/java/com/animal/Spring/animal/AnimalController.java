22f1beca1eeae56932273e281573c2390a84ead9
YousafTJ <youjav@hotmail.com>
2024-10-22 14:00:59 +0200

    Initial commit

---
+++ src/main/java/com/animal/Spring/animal/AnimalController.java
@@ -0,0 +1,34 @@
+package com.animal.Spring.animal;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AnimalController {
+
+    @GetMapping("/shark")
+    public String sharkPage() {
+        return "shark";  // This will look for shark.html in templates folder
+    }
+
+    @GetMapping("/lion")
+    public String lionPage() {
+        return "lion";   // This will look for lion.html in templates folder
+    }
+
+    @GetMapping("/eagle")
+    public String eaglePage() {
+        return "eagle";  // This will look for eagle.html in templates folder
+    }
+
+    @GetMapping("/icebear")
+    public String icebearPage() {
+        return "icebear";  // This will look for icebear.html in templates folder
+    }
+
+    @GetMapping("/gorilla")
+    public String gorillaPage() {
+        return "gorilla";  // This will look for gorilla.html in templates folder
+    }
+}
---
+++ src/main/java/com/animal/Spring/animal/SpringAnimalApplication.java
@@ -0,0 +1,13 @@
+package com.animal.Spring.animal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAnimalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAnimalApplication.class, args);
+	}
+
+}
